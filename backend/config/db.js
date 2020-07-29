973ed81f4ff030b2cf4b014550245471e122e9a0
김희윤 <dnflwlq3231@naver.com>
2020-07-29 17:57:52 +0900

    ADD config files

---
+++ backend/config/db.js
@@ -0,0 +1,152 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: '52.78.6.243',
+    port: 1823,
+    user: 'root',
+    password: '1234',
+    connectionLimit: 5    
+});
+
+// 총 구매 수량 체크
+async function getAllQuantaties() {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT SUM(quantity) FROM users');
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 휴대폰 번호로 지갑주소 가져오기
+async function getWalletAddress(phoneNumber) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT address FROM wallet WHERE phoneNumber=?', [phoneNumber])
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 1회차 유저정보 가져오기
+async function getUserInfoForFirstRound(phoneNumber) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT * FROM users WHERE phoneNumber=? AND round=1', [phoneNumber]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 2회차 유저정보 가져오기
+async function getUserInfoForSecondRound(phoneNumber) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT * FROM users WHERE phoneNumber=? AND round=2', [phoneNumber]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+} 
+
+// 몇번째 구매인지
+async function checkNumberOfPurchased(phoneNumber) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT COUNT(phoneNumber) FROM users WHERE phoneNumber=?', [phoneNumber]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 1라운드 순위
+async function getRankForFirstRound() {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT RANK() OVER (ORDER BY quantity desc) as ranking, phoneNumber, address FROM (SELECT phoneNumber, address, SUM(quantity) FROM users GROUP BY phoneNumber) WHERE round=1');
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 2라운드 순위
+async function getRankForSecondRound() {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('SELECT RANK() OVER (ORDER BY quantity desc) as ranking, phoneNumber, address FROM (SELECT phoneNumber, address, SUM(quantity) FROM users GROUP BY phoneNumber) WHERE round=2');
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 구매 기록하기
+async function setBuying(phoneNumber, quantity, place, round) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('INSERT INTO nodejs.users(phoneNumber, quantity, place, round)', [phoneNumber, quantity, place, round]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+}
+
+// 지갑 생성
+async function setWalletAddress(phoneNumber, address, publicKey) {
+    let conn, result;
+    try {
+        conn = await pool.getConnection();
+        conn.query('USE nodejs');
+        result = await conn.query('INSERT INTO nodejs.wallet(phoneNumber, address, publicKey)', [phoneNumber, address, publicKey]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+        return result;
+    }
+} 
+
+getRankForFirstRound();
+
+module.exports = { 
+    getAllQuantaties, getWalletAddress, getUserInfoForFirstRound, 
+    getUserInfoForSecondRound, checkNumberOfPurchased, getRankForFirstRound, 
+    getRankForSecondRound, setBuying, setWalletAddress
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,68 @@
-var express = require('express');
-var router = express.Router();
-var path = require("path");
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+const request = require('request');
+const config = require('../config/config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
+
+// 전화번호 입력 후 접속
+router.post('/api/authenticate', (req, res, next) => {
+  
+  // 전화번호에 지갑주소 있는지 확인
+  if (req.phoneNumber) {
+    res.json({
+      data: {
+        achievement: 10,
+        justEarned: false,
+        currentUser: {
+          phoneNumber: req.phoneNumber,
+          purchaseQuantity: {
+            firstRound: 1,
+            secondRound: 2
+          },
+          rewards: [
+            {
+              round: 'first',
+              type: 'free'
+            }
+          ]
+        }
+      }
+    })
+  } else {
+    request(creatingWalletOptions, (err, res) => {
+      if (err) {
+        console.log('creating wallet error', err)
+      } else {
+        console.log('creating wallet succeess', res.body)
+      }
+    })
+  }
+});
+
+// request options
+const creatingWalletOptions = {
+  method: "POST",
+  preambleCRLF: true,
+  postambleCRLF: true,
+  url: 'https://wallet-api.beta.klaytn.io/v2/account',
+  headers: {
+    'Content-type': 'application/json',
+    'x-krn': 'krn:1001:wallet:116:account:default',
+    'Authorization': 'Basic ' + config.kasAuth
+  }
+};
+
+// check date
+function checkDate() {
+  let today = new Date();
+  let month = today.getUTCMonth;
+  let date = today.getUTCDate;
+  return
+};
 
 module.exports = router;
