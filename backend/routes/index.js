3e641ba0ba5ab475dd14577832b8e8e7d5aeefcf
김희윤 <dnflwlq3231@naver.com>
2020-07-31 15:10:09 +0900

    UPDATE query

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,59 +6,59 @@ const config = require('../config/config.json');
 const db = require('../config/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
-	console.log('index page is open');
+  console.log('index page is open');
 });
 
 // 전화번호 입력 후 접속
-router.post('/api/authenticate', (req, res, next) => { 
-console.log(req.body);
- // 전화번호에 지갑주소 있는지 확인
+router.post('/api/authenticate', (req, res, next) => {
+  console.log(req.body);
+  // 전화번호에 지갑주소 있는지 확인
   db.getWalletAddress(req.body.phoneNumber)
     .then((result) => {
-	console.log(result);
-	if (result) {
+      console.log(result);
+      if (result) {
         console.log('get wallet success result ', result);
-	// 전체 구매 수 측정
+        // 전체 구매 수 측정
         db.getAllQuantaties().then((response) => {
           console.log('get all quantities ', response);
         }, (err) => {
           console.log('get quantaties error ', err)
         });
-  // 1회차 유저정보 호출
-  db.getUserInfoForFirstRound(req.body.phoneNumber)
-        .then((result) => {
-          console.log(result);
+        // 1회차 유저정보 호출
+        db.getUserInfoForFirstRound(req.body.phoneNumber)
+          .then((result) => {
+            console.log(result);
+          });
+        // 2회차 유저정보 호출
+        db.getUserInfoForSecondRound(req.body.phoneNumber)
+          .then((result) => {
+            console.log(result);
+          });
+        // 전화번호 존재하지 않으면      
+      } else {
+        request(creatingWalletOptions, (error, response, body) => {
+          let address, publicKey;
+          if (error) {
+            throw new Error(error);
+          } else if (response.statusCode == 200) {
+            JSON.parse(body).result.address = address;
+            JSON.parse(body).result.public_key = publicKey
+            db.setWalletAddress(req.phoneNumber, address, publicKey)
+              .then((err) => {
+                console.log('set wallet address error ', err);
+              }, (result) => {
+                console.log('set wallet address result', result);
+              });
+          } else {
+            console.log('fail to request creating wallet ', response);
+          }
         });
-  // 2회차 유저정보 호출
-  db.getUserInfoForSecondRound(req.body.phoneNumber)
-        .then((result) => {
-          console.log(result);
-        });
-    // 전화번호 존재하지 않으면      
-    } else {
-       request(creatingWalletOptions, (error, response, body) => {
-         if (error) {
-           throw new Error(error);
-         } else if (response.statusCode == 200) {
-           //response.body.result.address = address;
-           //response.body.result.public_key = publicKey;
-		console.log('parsing', JSON.parse(body).result);  
-	       db.setWalletAddress(req.phoneNumber, JSON.parse(body).result.address, JSON.parse(body).result.public_key)
-             .then((err) => {
-               console.log('set wallet address error ', err);
-             }, (result) => {
-		console.log('set wallet address result', result);
-	});
-         } else {
-           console.log('fail to request creating wallet ', response);
-         }
-       });
-    }
-  }, (err) => {
-    console.log('get wallet address error ', err);
-  });
+      }
+    }, (err) => {
+      console.log('get wallet address error ', err);
+    });
 });
 
 // 적립하는 api
@@ -69,7 +69,7 @@ router.post('/api/verify', (req, res, next) => {
 
       })
   } else {
-    req.json({msg: 'invalid password'});
+    req.json({ msg: 'invalid password' });
   }
 });
 
