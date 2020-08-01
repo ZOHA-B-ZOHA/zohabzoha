e720fdeb0efe5f6bea862e7c6be65f1031ad809a
김희윤 <dnflwlq3231@naver.com>
2020-08-02 01:29:23 +0900

    UPDATE contracts

--- backend/contracts/zohab.sol
+++ backend/contracts/zohab.sol
@@ -4,21 +4,70 @@ import "./KIP17.sol";
 
 contract zohabToken is KIP17 {
     
-    struct _token {
-        uint256 phoneNumber;
+    // 구매기록 저장
+    struct purchaseRecord {
+        address userAddress; // 유저 지갑주소
+        string marketPlace; // 구매 장소
+        uint24 count; // 구매 잔 수
     }
     
-    _token[] public incentiveList;
+    // 토큰들 저장
+    struct _roundOneFreeToken {
+        address account;
+    }
+    
+    struct _roundOnePlusToken {
+        address account;
+    }
+    
+    struct _roundTwoFreeToken {
+        address account;
+    }
+    
+    struct _roundTwoPlusToken {
+        address account;
+    }
+    
+    purchaseRecord[] public record;
+    _roundOneFreeToken[] public roundOneFreeList;
+    _roundOnePlusToken[] public roundOnePlusList;
+    _roundTwoFreeToken[] public roundTwoFreeList;
+    _roundTwoPlusToken[] public roundTwoPlusList;
     address public owner;
     
     constructor () public {
-        owner = msg.sender;
+        owner = msg.sender; // Token 발행 가능한 컨트랙트 소유자
     }
     
-    function mintToken(uint256 _phoneNumber, address account) public {
+    function updateRecord(address _userAddress, string memory place, uint24 count) public {
+        record.push(purchaseRecord(_userAddress, place, count));
+    }
+    
+    function mintRoundOneFreeToken(address account) public {
         require(owner == msg.sender);
-        uint256 tokenId = incentiveList.length;
-        incentiveList.push(_token(_phoneNumber));
+        uint256 tokenId = roundOneFreeList.length;
+        roundOneFreeList.push(_roundOneFreeToken(account));
+        _mint(account, tokenId);
+    }
+    
+    function mintRoundOnePlusToken(address account) public {
+        require(owner == msg.sender);
+        uint256 tokenId = roundOnePlusList.length;
+        roundOnePlusList.push(_roundOnePlusToken(account));
+        _mint(account, tokenId);
+    }
+    
+    function mintRoundTwoFreeToken(address account) public {
+        require(owner == msg.sender);
+        uint256 tokenId = roundTwoFreeList.length;
+        roundTwoFreeList.push(_roundTwoFreeToken(account));
+        _mint(account, tokenId);
+    }
+    
+    function mintRoundTwoPlusToken(address account) public {
+        require(owner == msg.sender);
+        uint256 tokenId = roundTwoPlusList.length;
+        roundTwoPlusList.push(_roundTwoPlusToken(account));
         _mint(account, tokenId);
     }
 }
