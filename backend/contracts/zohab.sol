e1bfacd98df3620eb7898a59514d34672cbeed22
김희윤 <dnflwlq3231@naver.com>
2020-08-03 18:50:48 +0900

    UPDATE contracts

--- backend/contracts/zohab.sol
+++ backend/contracts/zohab.sol
@@ -4,70 +4,102 @@ import "./KIP17.sol";
 
 contract zohabToken is KIP17 {
     
+    mapping (string => incentiveList) roundOnePlusTokenList;
+    mapping (string => incentiveList) roundOneFreeTokenList;
+    mapping (string => incentiveList) roundTwoPlusTokenList;
+    mapping (string => incentiveList) roundTwoFreeTokenList;
+    
+    mapping (address => purchaseRecord) roundOneRecordList;
+    mapping (address => purchaseRecord) roundTwoRecordList;
+    
+    event setRecordInList (address buyer, uint24 round, uint24 quantity);
+    event setTokenList (address account, uint256 tokenId);
+    
     // 구매기록 저장
     struct purchaseRecord {
-        address userAddress; // 유저 지갑주소
-        string marketPlace; // 구매 장소
         uint24 count; // 구매 잔 수
     }
     
     // 토큰들 저장
-    struct _roundOneFreeToken {
+    struct incentiveList {
         address account;
+        uint256 tokenId;
     }
     
-    struct _roundOnePlusToken {
-        address account;
-    }
-    
-    struct _roundTwoFreeToken {
-        address account;
-    }
-    
-    struct _roundTwoPlusToken {
-        address account;
-    }
-    
-    purchaseRecord[] public record;
-    _roundOneFreeToken[] public roundOneFreeList;
-    _roundOnePlusToken[] public roundOnePlusList;
-    _roundTwoFreeToken[] public roundTwoFreeList;
-    _roundTwoPlusToken[] public roundTwoPlusList;
     address public owner;
     
     constructor () public {
         owner = msg.sender; // Token 발행 가능한 컨트랙트 소유자
     }
     
-    function updateRecord(address _userAddress, string memory place, uint24 count) public {
-        record.push(purchaseRecord(_userAddress, place, count));
+    function updateRecord(address _userAddress, uint24 round, uint24 count) public {
+        if (round == 1) {
+            if (roundOneRecordList[_userAddress].count == 0) {
+                roundOneRecordList[_userAddress].count = count;
+            } else {
+                roundOneRecordList[_userAddress].count += count;
+            }
+        } else if (round == 2) {
+            if (roundTwoRecordList[_userAddress].count == 0) {
+                roundTwoRecordList[_userAddress].count = count;
+            } else {
+                roundTwoRecordList[_userAddress].count += count;
+            }
+        }
+        
+        emit setRecordInList(_userAddress, round, count);
     }
     
-    function mintRoundOneFreeToken(address account) public {
+    function mintToken(string memory phone, address account, uint256 tokenId, uint24 round, string memory couponType) public {
         require(owner == msg.sender);
-        uint256 tokenId = roundOneFreeList.length;
-        roundOneFreeList.push(_roundOneFreeToken(account));
+        string memory plus = 'plus';
+        string memory free = 'free';
         _mint(account, tokenId);
+        
+        if (round == 1) {
+            if (keccak256(bytes(plus)) == keccak256(bytes(couponType))) {
+                roundOnePlusTokenList[phone].account = account;
+                roundOnePlusTokenList[phone].tokenId = tokenId;
+            } else if (keccak256(bytes(free)) == keccak256(bytes(couponType))) {
+                roundOneFreeTokenList[phone].account = account;
+                roundOneFreeTokenList[phone].tokenId = tokenId;
+            }
+        } else if (round == 2) {
+            if (keccak256(bytes(plus)) == keccak256(bytes(couponType))) {
+                roundTwoPlusTokenList[phone].account = account;
+                roundTwoPlusTokenList[phone].tokenId = tokenId;
+            } else if (keccak256(bytes(free)) == keccak256(bytes(couponType))) {
+                roundTwoFreeTokenList[phone].account = account;
+                roundTwoFreeTokenList[phone].tokenId = tokenId;
+            }
+        }
+        
+        emit setTokenList(account, tokenId);
     }
     
-    function mintRoundOnePlusToken(address account) public {
-        require(owner == msg.sender);
-        uint256 tokenId = roundOnePlusList.length;
-        roundOnePlusList.push(_roundOnePlusToken(account));
-        _mint(account, tokenId);
+    function gasRecords(address _userAddress, uint24 round) public view returns(uint24) {
+        if (round == 1) {
+            return (
+                roundOneRecordList[_userAddress].count
+            );
+        } else if (round == 2) {
+            return (
+                roundTwoRecordList[_userAddress].count
+            );
+        }
     }
     
-    function mintRoundTwoFreeToken(address account) public {
-        require(owner == msg.sender);
-        uint256 tokenId = roundTwoFreeList.length;
-        roundTwoFreeList.push(_roundTwoFreeToken(account));
-        _mint(account, tokenId);
-    }
-    
-    function mintRoundTwoPlusToken(address account) public {
-        require(owner == msg.sender);
-        uint256 tokenId = roundTwoPlusList.length;
-        roundTwoPlusList.push(_roundTwoPlusToken(account));
-        _mint(account, tokenId);
+    function getTokenList(string memory phone, uint24 round) public view returns(uint256, uint256) {
+        if (round == 1) {
+            return (
+                roundOneFreeTokenList[phone].tokenId,
+                roundOnePlusTokenList[phone].tokenId
+            );
+        } else if (round == 2) {
+            return (
+                roundTwoFreeTokenList[phone].tokenId,
+                roundTwoPlusTokenList[phone].tokenId
+            );
+        }
     }
 }
