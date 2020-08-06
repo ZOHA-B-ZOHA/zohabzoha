91a9ec46bcc585508052f9fb95684f02bfde94b7
김희윤 <dnflwlq3231@naver.com>
2020-08-06 11:05:35 +0900

    APPLY .gitignore

--- backend/config/config.js
+++
@@ -1,7 +0,0 @@
-let auth = {
-    kasAuth:  "NWU1NDlmYjhiMjAyMzg1MzAwZjA4ODg3MGUxNTUzYTNmNjZmMmQwMzo0Y2I3YmUyNWJkZThkMzI5MmJjZTkzNmQ0MGNiMzMxOGM0OTY4MmM5",
-    qrCodePassword: "zohabzohafighting",
-    encryptKey: "zohabzohapassword"
-}
-
-module.exports = {auth}
